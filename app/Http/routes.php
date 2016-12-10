48a8022780cd17395edb875612fcf30993474e50
PavelBabakin <pavelbabakin7@gmail.com>
2016-12-10 10:29:37 +0200

    work with controllers and blade

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,14 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@home');
 
-Route::get('about', function (){
-    return view('pages.about');
-});
-
-Route::get('home', function (){
-    return view('pages.home');
-});
+Route::get('about', 'PagesController@about');
--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -38,7 +38,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Home Page</div>
+        <div class="title">@yield('content')</div>
     </div>
 </div>
 </body>
